7d1c1a21d2bdbeb0c3e83f1445bc9e4968311a40
ayaka <ayaka.hashimoto00@gmail.com>
2024-02-25 22:24:17 +0900

    first commit

---
+++ database/migrations/2024_02_25_114218_create_articles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->text('syouhinnmei');
+            $table->text('kakaku');
+            $table->text('zaikosuu');
+            $table->text('gyousya');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+};
